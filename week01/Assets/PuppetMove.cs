22047c7ed08603605bcff6b3bb009e34e0a767b8
Robert Yang <yang.robert.w@gmail.com>
2017-01-25 17:17:35 -0500

    week01: Unity review, prototype a cave allegory

---
+++ week01/Assets/PuppetMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuppetMove : MonoBehaviour {
+
+	float xDirection = 2f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		// Time.deltaTime = the duration of the last frame in seconds
+		// multiply by Time.deltaTime = makes a behavior framerate INDEPENDENT
+		transform.position += new Vector3(xDirection, 0f, 0f) * Time.deltaTime;
+
+		// turn around if you reach the edge of the cave
+		if ( transform.position.x > 2f || transform.position.x < -2f ) {
+			xDirection *= -1f; // multiply by -1f to flip the direction
+		}
+			
+	}
+}
