7e15597b639861ba13c4511ed2fd4d7be6d6de36
Robert Yang <yang.robert.w@gmail.com>
2017-02-01 16:14:24 -0500

    week02 VR intro: updated MyMouseLook example

--- week02_vrIntro/Assets/scripts/MyMouseLook.cs
+++ week02_vrIntro/Assets/scripts/MyMouseLook.cs
@@ -11,8 +11,14 @@ public class MyMouseLook : MonoBehaviour {
 		// are we using VR?
 		if ( VRDevice.isPresent == true ) {
 			// if so, print VR HMD model name to the console
-			Debug.Log("VR is enabled! " + VRDevice.model);
+			Debug.Log("VR is enabled! " + VRDevice.model );
+		} else {
+			Camera.main.transform.Translate(0f, 2f, 0f ); // move camera up if VR isn't enabled?
 		}
+
+		// render at 50% of the original resolution
+		// for faster performance and FPS inside the headset
+		VRSettings.renderViewportScale = 0.5f; 
 	}
 	
 	// Update is called once per frame
@@ -28,5 +34,8 @@ public class MyMouseLook : MonoBehaviour {
 			// unroll our camera's Z angle
 			myCam.localEulerAngles = new Vector3( myCam.localEulerAngles.x, myCam.localEulerAngles.y, 0f );
 		}
+
+		// write whatever code here afterwards
+
 	}
 }
